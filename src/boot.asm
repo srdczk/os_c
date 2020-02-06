27973c95292289c3654b666b7674b135fa48f8b4
srdczk <srdyjj@163.com>
2020-02-06 10:49:10 +0800

    no org boot loader

--- src/boot.asm
+++ src/boot.asm
@@ -1,25 +1,23 @@
-; a simple boot sector
-org 0x7c00
-
-mov ah, 0x0e ;tty mode
-mov al, 'n'
-int 0x10
-mov al, 'a'
-int 0x10
-mov al, 's'
-int 0x10
-mov al, 'i'
-int 0x10
-int 0x10
-mov al, 0x0a
-int 0x10
-
-jmp $  ;jump to cur addr -> loop
-
-times 510 - ($ - $$) db 0
-
-db 0x55
-db 0xaa
-
-;dw 0xaa55
-; 由于是小端 机器 , 所以 0xaa55 存储 -> 55 aa
+; boot.asm
+   mov ax, 0x07c0
+   mov ds, ax
+; 从 0x7c00 处开始 
+   mov si, msg
+   cld
+put_char:
+   or al, al ; if al == 0 end
+   jz loop   
+   mov ah, 0x0e
+   mov bh, 0
+   mov al, [si]
+   add si, 1
+   int 0x10
+   jmp put_char
+ 
+loop:
+   jmp loop
+ 
+msg:    
+    db "NIAMSILE", 0x0d, 0x0a, 0x00
+    times 510-($-$$) db 0
+    dw 0xaa55
