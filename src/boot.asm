685008e2ecf43aaf293e54c566675249e9966b61
srdczk <srdyjj@163.com>
2020-02-06 11:22:33 +0800

    call -ret version

--- src/boot.asm
+++ src/boot.asm
@@ -1,21 +1,27 @@
 [ORG 0x7c00]
-   xor ax, ax ; make it zero
-   mov ds, ax
-   mov si, msg
+    xor ax, ax
+    mov ds, ax
+    mov si, msg
+
+    call put_char
+
+loop:
+    jmp loop
+
+msg:
+    db "NIMASILE", 0x0d, 0x0a, 0x00
+
 put_char:
     mov al, [si]
     add si, 1
     or al, al
-    jz loop
+    jz done
     mov ah, 0x0e
-    mov bx, 0x00
+    mov bh, 0x00
     int 0x10
-   jmp put_char
- 
-loop:
-   jmp loop
- 
-msg:
-    db "NIMASILE", 0x0d, 0x0a, 0x00
+    jmp put_char
+done:
+    ret
+
     times 510 - ($ - $$) db 0
     dw 0xaa55
