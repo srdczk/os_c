30d81771f2d9cbc643b6f45f91750137c31f6673
srdczk <srdyjj@163.com>
2020-02-06 11:12:37 +0800

    org -version

--- src/boot.asm
+++ src/boot.asm
@@ -1,23 +1,21 @@
-; boot.asm
-   mov ax, 0x07c0
+[ORG 0x7c00]
+   xor ax, ax ; make it zero
    mov ds, ax
-; 从 0x7c00 处开始 
    mov si, msg
-   cld
 put_char:
-   or al, al ; if al == 0 end
-   jz loop   
-   mov ah, 0x0e
-   mov bh, 0
-   mov al, [si]
-   add si, 1
-   int 0x10
+    mov al, [si]
+    add si, 1
+    or al, al
+    jz loop
+    mov ah, 0x0e
+    mov bx, 0x00
+    int 0x10
    jmp put_char
  
 loop:
    jmp loop
  
-msg:    
-    db "NIAMSILE", 0x0d, 0x0a, 0x00
-    times 510-($-$$) db 0
+msg:
+    db "NIMASILE", 0x0d, 0x0a, 0x00
+    times 510 - ($ - $$) db 0
     dw 0xaa55
