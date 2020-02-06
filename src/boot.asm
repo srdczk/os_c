2fcdda8377ba33b316d6d8fcaaaa7ccb34bb56f7
srdczk <srdyjj@163.com>
2020-02-06 09:48:31 +0800

    boot sector print

--- src/boot.asm
+++ src/boot.asm
@@ -1,34 +1,25 @@
-; A simple boot sector 
-org 0x7c00 ;load from 0x7c00
+; a simple boot sector
+org 0x7c00
 
-entry:
-    mov ax, 0
-    mov ss, ax
-    mov ds, ax
-    mov es, ax
-    mov si, msg
+mov ah, 0x0e ;tty mode
+mov al, 'n'
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 's'
+int 0x10
+mov al, 'i'
+int 0x10
+int 0x10
+mov al, 0x0a
+int 0x10
 
-putloop:
-    mov al, [si]
-    add si, 1
-    cmp al, 0
-    je fin
-    mov ah, 0x0e
-    mov bx, 15
-    int 0x10
-    jmp putloop
+jmp $  ;jump to cur addr -> loop
 
-fin:
-    HLT
-    jmp fin
+times 510 - ($ - $$) db 0
 
-msg:
-    db 0x0a, 0x0a
-    db "NIMASILE"
-    db 0xa
-    db 0
+db 0x55
+db 0xaa
 
-times 510-($-$$) db 0
-
-
-dw 0xaa55
+;dw 0xaa55
+; 由于是小端 机器 , 所以 0xaa55 存储 -> 55 aa
