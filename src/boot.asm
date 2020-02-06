97e032b9011725501b4f153a60b6605e090e3549
srdczk <srdyjj@163.com>
2020-02-06 11:25:52 +0800

    macro version

--- src/boot.asm
+++ src/boot.asm
@@ -1,16 +1,5 @@
-[ORG 0x7c00]
-    xor ax, ax
-    mov ds, ax
-    mov si, msg
-
-    call put_char
-
-loop:
-    jmp loop
-
-msg:
-    db "NIMASILE", 0x0d, 0x0a, 0x00
-
+%macro Print 1
+    mov si, %1
 put_char:
     mov al, [si]
     add si, 1
@@ -22,6 +11,17 @@ put_char:
     jmp put_char
 done:
     ret
+%endmacro
+[ORG 0x7c00]
+    xor ax, ax
+    mov ds, ax
+    Print msg
+loop:
+    jmp loop
+
+msg: 
+    db "NIMASILE", 0x0d, 0x0a, 0x00
 
     times 510 - ($ - $$) db 0
     dw 0xaa55
+        
